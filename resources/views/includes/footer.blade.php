bfa4884d52d9fb93d09fc49335b157dcb7ad497d
tranhao98 <tranhao050598@gmail.com>
2021-07-16 23:15:00 +0700

    Update footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,9 +1,8 @@
-<div class="container">
+
     <footer class="row mt-3">
-        <div style="background-color: #cccc;" class="w-100 p-2 rounded">
-            <div class="d-flex justify-content-center align-items-center">
+        <div class="w-100 p-2 rounded bg-dark">
+            <div class="text-light d-flex justify-content-center align-items-center">
                 <p class="mb-0">Long Trấn Hào &copy; 2021. All rights reserved</p>
             </div>
         </div>
     </footer>
-</div>
